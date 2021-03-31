90b319465f32dd55ce607c5801e8bbe2e319dbec
Vinicius Moreira <viniciuspgmoreira@hotmail.com>
2021-03-31 14:04:43 -0300

    updating app with shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -2,27 +2,7 @@ import React from 'react'
 import '../src/App.css'
 import HomePage from '../src/pages/homepage/homepage.component'
 import { Route, Switch } from 'react-router-dom'
-
-
-const Topics = (props) => (
-  <div>
-    <h1>Topics</h1>
-    <button onClick={() => props.history.push('/')}>Click</button>
-
-  </div>
-)
-
-const TopcisWithParams = (props) => {
-
-  console.log(props)
-  return (
-    <div >
-      <h2>Topic Details Page: {props.match.params.topicId}</h2>
-    </div>
-
-  )
-}
-
+import ShopPage from '../src/pages/shop/shop.component.jsx';
 
 
 function App() {
@@ -31,8 +11,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
-        <Route path='/topics' component={Topics}></Route>
-        <Route path='/topics/:topicId' component={TopcisWithParams}></Route>
+        <Route path='/shop' component={ShopPage}></Route>
       </Switch>
     </div>
   );
